b0fc7a1f7fbd42bfe66cd2c488e40aacabf073ae
4emar <stojanovicmarko32@gmail.com>
2022-08-15 00:20:10 +0200

    without import mapbox-gl/dist/mapbox-gl.css, so the popups and markers will not work

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import getCenter from "geolib/es/getCenter";
-import "mapbox-gl/dist/mapbox-gl.css";
+//import "mapbox-gl/dist/mapbox-gl.css";
 import PopupCard from './PopupCard';
 
 function Map({ searchResults }) {
